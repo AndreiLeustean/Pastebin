860384465a704e50d6f734ea0642c9c66aaaba6a
AndreiLeustean <163894215+AndreiLeustean@users.noreply.github.com>
2024-12-07 19:04:48 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,10 +7,19 @@ const port = 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('view'));
+
+app.set('view engine', 'ejs');
+app.set('views', './view');
 
 app.post('/text/save', handleSaveText);
 app.get('/text/last-id', handleGetLastId);
 app.get('/texts', handleGetAllTexts);
+
+app.get('/', (req, res) => {
+    const data = { name: 'Pastebin User' };
+    res.render('index', data);
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
